ec49e2a02cef2473b81d4113b16b4eee64d5e59b
Tyler Ferrin <tyferrin@gmail.com>
2017-10-19 18:55:04 -0600

    init commit

---
+++ components/Masthead.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mh__container">
+    <div class="mh__logo-container">
+      <img src="~/assets/TYPHOON_OFFERINGS_TYPEFACE.svg" alt="TYPHOON">
+    </div>
+  </div>
+</template>
+<style lang="sass">
+
+.mh
+  &__container
+    display: block
+    width: 100vw
+    height: calc(100vh / 5)
+    background-color: white
+  &__logo-container
+    display: flex
+    flex-direction: row
+    justify-content: flex-start
+    img
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <Masthead />
+    <div class="body">
+
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Masthead from '~/components/Masthead.vue'
+
+export default {
+  components: {
+    Logo,
+    Masthead
+  }
+}
+</script>
+
+<style lang="sass">
+.body
+  height: 80vh
+  display: block
+  background-image: url('~/assets/typhoon_press_photo.jpg')
+  background-position: center
+  background-size: contain
+  background-repeat: no-repeat
+</style>
