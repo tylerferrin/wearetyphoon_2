eac54db8a9916e2514a1b7aa387c007c37aacef1
Tyler Ferrin <tyferrin@gmail.com>
2018-04-16 15:26:57 -0700

    test new config.yml file

--- .circleci/automated_build.sh
+++ .circleci/automated_build.sh
@@ -1,9 +1,9 @@
-echo ### Generating new static files ###
-npm run generate
-
-git config --global user.email "tyferrin@gmail.com"
-git config --global user.name "Tyler Ferrin"
-
-git add .
-git commit -m "Automated Rebuild"
-git push
+# echo ### Generating new static files ###
+# npm run generate
+#
+# git config --global user.email "tyferrin@gmail.com"
+# git config --global user.name "Tyler Ferrin"
+#
+# git add .
+# git commit -m "Automated Rebuild"
+# git push
